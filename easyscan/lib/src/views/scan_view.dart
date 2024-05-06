097467d2c086bca905c4d2f7dc8733141f9fac13
WinterBoii <wintercodess@gmail.com>
2024-05-06 23:26:22 +0200

    feat: qr screen upgrade

--- easyscan/lib/src/views/scan_view.dart
+++ easyscan/lib/src/views/scan_view.dart
@@ -1,104 +1,120 @@
-import 'package:easyscan/src/scan/flashlight.dart';
-import 'package:easyscan/src/scan/scanner_barcode_label.dart';
-import 'package:easyscan/src/scan/scanner_error_widget.dart';
-import 'package:easyscan/src/scan/switch_camera.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:vibration/vibration.dart';
 
 class ScanView extends StatefulWidget {
   const ScanView({super.key});
-
-  static const routeName = '/scan';
-
   @override
-  State<ScanView> createState() => _BarcodeScannerWithOverlayState();
+  State<ScanView> createState() => _ScanViewState();
+
+  static const routeName = '/scan_view';
 }
 
-class _BarcodeScannerWithOverlayState extends State<ScanView> {
-  final MobileScannerController controller = MobileScannerController(
-    formats: const [BarcodeFormat.qrCode],
-  );
+class _ScanViewState extends State<ScanView> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+  bool scanStarted = false;
+  bool processingCode = false;
+  String? scannedData;
 
+  // Cache for scan results
+  final Map<String, dynamic> _scanCache = {};
+
+  /// Resumes the camera when the widget is initialized.
+  ///
+  /// This method is called when the widget is first created. It resumes the camera
+  /// that was previously paused, allowing the QR code scanner to start capturing
+  /// images again.
   @override
   void initState() {
     super.initState();
-    controller.start();
+    controller?.resumeCamera();
+  }
+
+  @override
+  void dispose() {
+    /// Pauses the camera and disposes of the QRViewController.
+    /// This is called when the widget is being disposed of.
+    controller?.pauseCamera();
+    controller?.dispose();
+    super.dispose();
   }
 
   @override
 
-  /// Builds the scan view UI, including a semi-transparent background with a cutout area for the scan window, and a border around the scan window.
+  /// Pauses the camera when the widget is reassembled.
   ///
-  /// The `build` method is responsible for constructing the visual elements of the scan view. It creates a `Scaffold` with an `AppBar` and a `Stack` of widgets, including:
-  /// - A `MobileScanner` widget that displays the camera feed and handles barcode scanning.
-  /// - A `CustomPaint` widget that renders the scan window overlay using the `ScannerOverlay` custom painter.
-  /// - Buttons for toggling the flashlight and switching the camera.
-  ///
-  /// The method uses the `scanWindow` variable to determine the size and position of the scan window, and the `controller` variable to interact with the `MobileScanner` widget.
+  /// This method is called when the widget is reassembled, such as when the
+  /// widget's state changes or the widget is moved in the widget tree. It
+  /// checks if the [controller] is not null, and if so, pauses the camera.
+  /// This ensures that the camera is not running when the widget is not
+  /// visible, which can help conserve device resources.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller?.pauseCamera();
+    }
+    controller?.resumeCamera();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final scanWindow = Rect.fromCenter(
-      center: MediaQuery.sizeOf(context).center(Offset.zero),
-      width: MediaQuery.sizeOf(context).width * 0.75,
-      height: MediaQuery.sizeOf(context).width * 0.75,
-    );
+    final width = MediaQuery.of(context).size.width;
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Skanna produkt'),
       ),
-      body: Stack(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Center(
-            child: MobileScanner(
-              fit: BoxFit.cover,
-              controller: controller,
-              scanWindow: scanWindow,
-              errorBuilder: (context, error, child) {
-                return ScannerErrorWidget(error: error);
-              },
-              overlayBuilder: (context, constraints) {
-                return Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: ScannedBarcodeLabel(barcodes: controller.barcodes),
+          Flexible(
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Stack(
+                children: [
+                  Container(
+                    width: width,
+                    height: width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                    child: _buildQrView(context),
                   ),
-                );
-              },
+                  Visibility(
+                    visible: processingCode,
+                    child: Stack(
+                      children: <Widget>[
+                        Opacity(
+                          opacity: 0.7,
+                          child: Container(
+                            color: Colors.black,
+                          ),
+                        ),
+                        const Center(
+                          child: CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-
-          /// Renders a custom paint widget that displays a scanner overlay on the screen.
-          ///
-          /// The `ValueListenableBuilder` widget listens to the `controller` value and only renders the `CustomPaint` widget when the camera is initialized, running, and has no errors.
-          ///
-          /// The `CustomPaint` widget uses the `ScannerOverlay` custom painter to draw a semi-transparent background with a cutout area for the scan window, and a border around the scan window.
-          ///
-          /// The `scanWindow` property is used to determine the size and position of the scan window within the custom paint widget.
-          ValueListenableBuilder(
-            valueListenable: controller,
-            builder: (context, value, child) {
-              if (!value.isInitialized ||
-                  !value.isRunning ||
-                  value.error != null) {
-                return const SizedBox();
-              }
-
-              return CustomPaint(
-                painter: ScannerOverlay(scanWindow: scanWindow),
-              );
-            },
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ToggleFlashlightButton(controller: controller),
-                  SwitchCameraButton(controller: controller),
-                ],
+          Padding(
+            padding: const EdgeInsets.only(bottom: 40, left: 16, right: 16),
+            child: Center(
+              child: Text(
+                scannedData ?? 'No display value.',
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.fade,
+                style: const TextStyle(color: Colors.white, fontSize: 20),
               ),
             ),
           ),
@@ -107,83 +123,150 @@ class _BarcodeScannerWithOverlayState extends State<ScanView> {
     );
   }
 
-  @override
-  Future<void> dispose() async {
-    super.dispose();
-    await controller.dispose();
+  /// Builds a QR code scanner view with a customized overlay.
+  /// The [QRView] widget is used to create the QR code scanner view. The overlay is
+  /// customized using the [QrScannerOverlayShape] widget, which sets the border
+  /// color, radius, length, width, and the cutout size.
+  ///
+  /// The [_buildQrView] method is responsible for creating the QR code scanner
+  /// view and its overlay.
+  Widget _buildQrView(BuildContext context) {
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+        borderColor: Colors.greenAccent,
+        borderRadius: 10,
+        borderLength: 30,
+        borderWidth: 10,
+        cutOutSize: MediaQuery.of(context).size.width * 0.8,
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+      scanStarted = true;
+    });
+
+    /// The code snippet `controller.scannedDataStream.listen((scanData) { ... });` sets up a listener
+    /// on the `scannedDataStream` stream provided by the `QRViewController` instance. This listener
+    /// listens for incoming scan data from the QR code scanner.
+    controller.scannedDataStream.listen((scanData) {
+      Vibration.hasVibrator().then((hasVibrator) {
+        if (hasVibrator! && scanData.code!.isNotEmpty) {
+          setState(() {
+            processingCode = true;
+          });
+          Vibration.vibrate(duration: 500);
+        }
+      });
+      // Validate input
+      if (_isScanDataValid(scanData)) {
+        _logScanResult(scanData);
+        _handleScanData(scanData);
+        setState(() {
+          processingCode = false;
+        });
+      } else {
+        _handleInvalidScanData(scanData);
+        setState(() {
+          processingCode = false;
+        });
+      }
+    });
+  }
+
+  bool _isScanDataValid(dynamic scanData) {
+    if (scanData == null || scanData.code == null) {
+      return false;
+    }
+
+    if (scanData.code.length < 5) {
+      return false;
+    }
+
+    return true;
+  }
+
+  void _logScanResult(dynamic scanData) {
+    setState(() {
+      scannedData = scanData.code;
+      controller?.pauseCamera();
+    });
+  }
+
+  void _handleInvalidScanData(dynamic scanData) {
+    setState(() {
+      processingCode = false;
+      scannedData = scanData.code;
+      controller?.resumeCamera();
+    });
+  }
+
+  /// Handles the processing of scan data received from a scanning operation.
+  ///
+  /// This method first checks if the scan data has already been cached. If so, it uses the cached result.
+  /// Otherwise, it stores the scan data in the cache for future use.
+  ///
+  /// The method then processes the scan data based on the value of the `code` property. If the code matches
+  /// 'case1', it calls the `_handleCase1` method. If the code matches 'case2', it calls the `_handleCase2`
+  /// method. For any other code, it calls the `_handleDefaultCase` method.
+  ///
+  /// The `scanData` parameter is of type `dynamic`, which means it can be any type of data. The specific
+  /// structure and properties of the `scanData` object are not documented here, as they are likely
+  /// implementation details of the scanning functionality.
+  void _handleScanData(dynamic scanData) {
+    String scanKey = scanData.code;
+
+    if (_scanCache.containsKey(scanKey)) {
+      print('Using cached result for: $scanKey');
+      scanData = _scanCache[scanKey];
+    } else {
+      _scanCache[scanKey] = scanData; //[Key, Value]
+    }
+
+    switch (scanData.code) {
+      case 'case1':
+        _handleCase1(scanData);
+        break;
+
+      case 'case2':
+        _handleCase2(scanData);
+        break;
+
+      default:
+        _handleDefaultCase(scanData);
+        break;
+    }
+  }
+
+  void _handleCase1(dynamic scanData) {
+    // Case 1 logic
+  }
+
+  void _handleCase2(dynamic scanData) {
+    // Case 2 logic
+  }
+
+  void _handleDefaultCase(dynamic scanData) {
+    // Default case logic
   }
 }
 
-class ScannerOverlay extends CustomPainter {
-  const ScannerOverlay({
-    required this.scanWindow,
-    this.borderRadius = 18.0,
-  });
 
-  final Rect scanWindow;
-  final double borderRadius;
 
-  @override
-
-  /// Paints the scan view on the canvas, including a semi-transparent background with a cutout area for the scan window, and a border around the scan window.
-  ///
-  /// The `paint` method is responsible for rendering the visual elements of the scan view on the canvas. It creates a background path that covers the entire canvas, and then subtracts a cutout path that corresponds to the scan window. This creates a semi-transparent background with a clear area for the scan window. It then draws a border around the scan window using a green accent color.
-  ///
-  /// The method takes the following parameters:
-  /// - `canvas`: The canvas on which to draw the scan view.
-  /// - `size`: The size of the canvas.
-  ///
-  /// The method uses the `scanWindow` and `borderRadius` properties to determine the size and shape of the scan window and its border.
-  void paint(Canvas canvas, Size size) {
-    // TODO: use `Offset.zero & size` instead of Rect.largest
-    // we need to pass the size to the custom paint widget
-    final backgroundPath = Path()..addRect(Rect.largest);
-
-    final cutoutPath = Path()
-      ..addRRect(
-        RRect.fromRectAndCorners(
-          scanWindow,
-          topLeft: Radius.circular(borderRadius),
-          topRight: Radius.circular(borderRadius),
-          bottomLeft: Radius.circular(borderRadius),
-          bottomRight: Radius.circular(borderRadius),
-        ),
-      );
-
-    final backgroundPaint = Paint()
-      ..color = Colors.black.withOpacity(0.5)
-      ..style = PaintingStyle.fill
-      ..blendMode = BlendMode.dstOut;
-
-    final backgroundWithCutout = Path.combine(
-      PathOperation.difference,
-      backgroundPath,
-      cutoutPath,
-    );
-
-    final borderPaint = Paint()
-      ..color = Colors.greenAccent
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 4.0;
-
-    final borderRect = RRect.fromRectAndCorners(
-      scanWindow,
-      topLeft: Radius.circular(borderRadius),
-      topRight: Radius.circular(borderRadius),
-      bottomLeft: Radius.circular(borderRadius),
-      bottomRight: Radius.circular(borderRadius),
-    );
-
-    // First, draw the background,
-    // with a cutout area that is a bit larger than the scan window.
-    // Finally, draw the scan window itself.
-    canvas.drawPath(backgroundWithCutout, backgroundPaint);
-    canvas.drawRRect(borderRect, borderPaint);
-  }
-
-  @override
-  bool shouldRepaint(ScannerOverlay oldDelegate) {
-    return scanWindow != oldDelegate.scanWindow ||
-        borderRadius != oldDelegate.borderRadius;
-  }
-}
+/* Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 50, left: 24, right: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ToggleFlashlightButton(controller: controller),
+                  SwitchCameraButton(controller: controller),
+                ],
+              ),
+            ),
+          ), */
